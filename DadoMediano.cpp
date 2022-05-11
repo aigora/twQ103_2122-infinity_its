5eb459d716442e933e7d7f705b54caf293e4d5d9
IreneRico <100791927+IreneRico@users.noreply.github.com>
2022-05-11 12:45:54 +0200

    Create DadoMediano.cpp

---
+++ DadoMediano.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <cstdlib>
+#include<ctime>
+int DadoDiminuto (int x){
+	int i; 
+	int dado; 
+	printf("Pulse '0' para lanzar el dado\n"); 
+	scanf("%d", &x); 
+	if (x==0){
+			srand (time(0)); 
+		for (i=0; i<7; i++){
+			dado= 1+ (rand ()%5); 
+		}
+	}
+	return dado; 
+}
